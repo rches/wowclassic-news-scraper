0a8ff3ddcf611d490e4fb3890cd9654d4b80650f
Robert Cheseldine <robert.cheseldine@gmail.com>
2019-11-17 12:14:35 -0500

    updating with user data and forum post logic

--- main.js
+++ main.js
@@ -1,34 +1,45 @@
-const Apify = require('apify');
+const Apify = require("apify");
 
 //list global variables
 
 Apify.main(async () => {
-    // Apify.openRequestQueue() is a factory to get a preconfigured RequestQueue instance.
-    // We add our first request to it - the initial page the crawler will visit.
     const requestQueue = await Apify.openRequestQueue();
-    for (i=0; i<7; i++) {
-        await requestQueue.addRequest({ url: `https://us.forums.blizzard.com/en/wow/c/wow-classic/l/latest.json?page=${i}` });
 
-
+    for (i = 0; i < 7; i++) {
+        await requestQueue.addRequest({
+            url: `https://us.forums.blizzard.com/en/wow/c/wow-classic/l/latest.json?page=${i}`,
+            userData: { requestType: "getID" }
+        });
     }
 
-    // Create an instance of the PuppeteerCrawler class - a crawler
-    // that automatically loads the URLs in headless Chrome / Puppeteer.
     const crawler = new Apify.PuppeteerCrawler({
         requestQueue,
         handlePageFunction: async ({ page, request }) => {
-            // This function is called to extract data from a single web page
-            // 'page' is an instance of Puppeteer.Page with page.goto(request.url) already called
-            // 'request' is an instance of Request class with information about the page to load
-
             let scrapedJson;
-            
+            if (request.userData.requestType === "getID") {
+                try {
+                    scrapedJson = JSON.parse(
+                        await page.$eval(`body`, el => el.innerText)
+                    );
+                } catch (e) {
+                    console.log(`JSON Failure: ${e}`);
+                }
 
-            try {scrapedJson = JSON.parse(await page.$eval(`body`, el => el.innerText));
-            console.log(scrapedJson);
-        } catch (e) {
-            console.log(`JSON Failure: ${e}`)
-        }
+                for (i = 0; i < scrapedJson.topic_list.topics.length; i++) {
+                    let forumPostURL = `https://us.forums.blizzard.com/en/wow/t/${scrapedJson.topic_list.topics[i].id}`;
+                    await requestQueue.addRequest({
+                        url: forumPostURL,
+                        userData: { requestType: "getForumData" }
+                    });
+                    console.log(forumPostURL);
+                }
+            }
+
+            if (request.userData.requestType === "getForumData") {
+                console.log(
+                    `You are looking at forum posts, my dude! -- ${request.url}`
+                );
+            }
 
             // await Apify.pushData({
             //     title: scrapedJson.topic_list.topics.fancy_title,
@@ -41,15 +52,12 @@ Apify.main(async () => {
             await Apify.pushData({
                 url: request.url,
                 succeeded: false,
-                errors: request.errorMessages,
+                errors: request.errorMessages
             });
-        },
+        }
     });
-    
+
     await crawler.run();
 
-    // Run the crawler and wait for it to finish.
-    await crawler.run();
-
-    console.log('Crawler finished.');
+    console.log("Crawler finished.");
 });
